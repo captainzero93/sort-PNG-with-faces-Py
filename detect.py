49ab0925759c08baa377216106455e432224d267
captainzero93 <125401979+captainzero93@users.noreply.github.com>
2023-02-15 11:31:45 +0000

    Update detect.py

--- detect.py
+++ detect.py
@@ -9,10 +9,14 @@ model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 # Define the input and output folders
 input_folder = '.'
 output_folder = 'output'
+output_folder2 = 'output2'
 
-# Create the output folder if it doesn't exist
+# Create the output folders if they don't exist
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
+
+if not os.path.exists(output_folder2):
+    os.makedirs(output_folder2)
 
 def process_image(filename):
     if filename.endswith('.png'):
@@ -27,10 +31,15 @@ def process_image(filename):
         faces = results.xyxy[0]
         print(f"Detected faces: {faces}")
 
-        # If there are faces detected, move the image to the output folder
-        if len(faces) > 0:
+        # If there is only one face detected, move the image to the output folder
+        if len(faces) == 1:
             os.rename(os.path.join(input_folder, filename), os.path.join(output_folder, filename))
             print(f"Moved image to output folder: {filename}")
+
+        # If there are multiple faces detected, move the image to the output2 folder
+        elif len(faces) > 1:
+            os.rename(os.path.join(input_folder, filename), os.path.join(output_folder2, filename))
+            print(f"Moved image to output2 folder: {filename}")
 
 # Create a list of filenames in the input folder
 filenames = [filename for filename in os.listdir(input_folder) if filename.endswith('.png')]
